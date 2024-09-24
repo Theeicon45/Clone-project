db7adaaecaef314f7ceafb08e29914d011d41a11
Theeicon45 <judahkinyanjui@gmail.com>
2024-09-24 14:15:49 +0300

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     < >
      <div className='flex items-center gap-14 '>
      <img src={navImg} alt="" className='w-40 cursor-pointer mx-32 ' />
-      <div id="navlist" className='flex items-center  gap-10 cursor-pointer font-bold uppercase'>
+      <div id="navlist" className='flex items-center  gap-8 cursor-pointer font-bold uppercase'>
         {
           navLists.map((nav)=> (
             <div key={nav}>
