72ec4d4d7e72651efa55475566c8d40107e9dd24
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-09-24 20:07:35 +0300

    adding padding

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="mt-auto bg-brown" >
+        <footer className="mt-auto bg-brown p-5" >
             <div className='flex items-center justify-center background'>
                 <img src="/images/instagram.svg" alt="instagram icon" className='h-7 cursor-pointer mx-30  '/>
                 <img src="/images/facebook.svg" alt="facebook icon" className='h-7 cursor-pointer mx-30 '/>
