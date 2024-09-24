a0f6ce400eb2dd078ca1762256079682d9e5b462
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-09-24 19:54:37 +0300

    changing instagram icon

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="mt-auto bg-brown" >
             <div className='flex items-center justify-center background'>
-                <img src="/images/instagram.svg" alt="instagram icon" className='h-12 cursor-pointer mx-30  '/>
+                <img src="/images/instagram.svg" alt="instagram icon" className='h-7 cursor-pointer mx-30  '/>
                 <img src="/images/facebook.svg" alt="facebook icon" className='h-7 cursor-pointer mx-30 '/>
                 <img src="/images/youtube.svg" alt="youtube icon" className='h-7 cursor-pointer mx-30 '/>
             </div>
