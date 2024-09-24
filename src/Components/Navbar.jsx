647cbe0c62fc8198ea528182e5e2a1ae8bba0db7
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-09-24 19:27:21 +0300

    adding icons fo the footer

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     < >
      <div className='flex items-center gap-14 '>
-     <img src={navImg} alt="" className='w-40 cursor-pointer mx-32 ' />
+     <img src={navImg} alt="organization logo" className='w-40 cursor-pointer mx-32 ' />
       <div id="navlist" className='flex items-center  gap-8 cursor-pointer font-bold uppercase'>
         {
           navLists.map((nav)=> (
---
+++ src/Components/footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Thank you for visiting!</p>
+            <img src="/images/instagram.svg" alt="Description of the image" className='w-10 cursor-pointer mx-30 '/>
+            <img src="/images/facebook.svg" alt="Description of the image" className='w-10 cursor-pointer mx-30 '/>
+            <img src="/images/youtube.svg" alt="Description of the image" className='w-10 cursor-pointer mx-30 '/>
+        </footer>
+    );
+};
+
+export default Footer;
